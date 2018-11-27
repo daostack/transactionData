e17624a73ff42738fc027c7512f51755f0151f0a
Adam Levi <leviadam@gmail.com>
2018-11-27 15:46:31 +0200

    Allow different decimals

--- tokenTransferData.js
+++ tokenTransferData.js
@@ -1,14 +1,16 @@
 const web3 = require('web3');
+const BN = web3.utils.BN;
 
-module.exports.tokenTransferData = (amountGen, address) => {
+module.exports.tokenTransferData = (amountToken, address, decimals) => {
     const transferFunSig = "a9059cbb";
-    const amountGenWei = web3.utils.toWei(amountGen.toString());
-    const amountGenWeiHex = web3.utils.toHex(amountGenWei);
-    const amountGenWeiHexNoPrefix = amountGenWeiHex.substring(2);
-    const amountGenWeiHexPadded = amountGenWeiHexNoPrefix.padStart(64, "0");
+    console.log(amountToken, address, decimals);
+    const amountTokenBN = (new BN(amountToken)).mul((new BN(10)).pow(new BN(decimals)));
+    const amountTokenBNHex = web3.utils.toHex(amountTokenBN);
+    const amountTokenBNHexNoPrefix = amountTokenBNHex.substring(2);
+    const amountTokenBNHexPadded = amountTokenBNHexNoPrefix.padStart(64, "0");
     const addressNoPrefix = address.substring(2);
     const addressPadded = addressNoPrefix.padStart(64, "0");
-    const fullTX = "0x" + transferFunSig + addressPadded + amountGenWeiHexPadded;
+    const fullTX = "0x" + transferFunSig + addressPadded + amountTokenBNHexPadded;
 
     return fullTX;
 }
